fa879be36668f432f88bdce1e423c466aacf23e7
emanb29 <eb@ethanbell.me>
2020-04-24 23:02:49 -0700

    Add a navigation

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,53 @@
 <template>
-  <div class="container">
-    <div class="w-100">
-      <h1 class="title display-1">
-        Replay My Career
-        <br />
-        <small class="text-muted">A Card-Based Portfolio</small>
-      </h1>
-      <section class="about">
-        <header class="h1">About Me</header>
-        <p class="lead">Here's a bit of stuff about me</p>
-      </section>
-      <section class="projects">
-        <header class="h1">My Projects</header>
-        <br />
-        <project-deck :projects="projects"></project-deck>
-      </section>
-      <section class="contact">
-        <header class="h1">Contact</header>
-        <p>[Contact info here]</p>
-      </section>
-      <div class="links mb-3">
-        <a href="https://nuxtjs.org/" target="_blank" class="btn btn-primary">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="btn btn-secondary"
-        >
-          GitHub
-        </a>
+  <div id="top" class="page position-relative">
+    <b-navbar
+      type="light"
+      class="bg-white border-bottom"
+      toggleable="md"
+      sticky
+    >
+      <b-navbar-brand href="#top">Portfolio</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#about">About</b-nav-item>
+          <b-nav-item href="#projects">Projects</b-nav-item>
+          <b-nav-item href="#contact">Contact</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <div class="container">
+      <div class="w-100">
+        <h1 class="title display-1">
+          Replay My Career
+          <br />
+          <small class="text-muted">A Card-Based Portfolio</small>
+        </h1>
+        <section id="about" class="about">
+          <header class="h1">About Me</header>
+          <p class="lead">Here's a bit of stuff about me</p>
+        </section>
+        <section id="projects" class="projects">
+          <header class="h1">My Projects</header>
+          <br />
+          <project-deck :projects="projects"></project-deck>
+        </section>
+        <section id="contact" class="contact">
+          <header class="h1">Contact</header>
+          <p>[Contact info here]</p>
+        </section>
+        <div class="links mb-3">
+          <a href="https://nuxtjs.org/" target="_blank" class="btn btn-primary">
+            Documentation
+          </a>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            class="btn btn-secondary"
+          >
+            GitHub
+          </a>
+        </div>
       </div>
     </div>
   </div>
@@ -94,6 +112,9 @@ export default Vue.extend({
   justify-content: center;
   align-items: center;
   text-align: center;
+}
+:target {
+  padding-top: 57px + 10px; // 57px is the magic value for the height of the navbar
 }
 
 .links {
