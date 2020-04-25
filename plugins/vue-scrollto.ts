b7fa587be89aabd802fc69930815b9f00232a346
emanb29 <emanb29@users.noreply.github.com>
2020-04-25 08:35:59 +0000

    deploy: e0fa914b28c4144ccf33f6ce1a5d4bd2f90bc9ee

--- plugins/vue-scrollto.ts
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import VueScrollTo from 'vue-scrollto'
-
-Vue.use(VueScrollTo, {
-  onDone(el: Element) {
-    // when scrolling within a page, don't push to history or jump around when offset is set
-    history.replaceState({}, document.title, `#${el.id}`)
-  }
-})
